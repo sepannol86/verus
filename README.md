350ace399f5a81d4f0ba04b5edfaa225e8d71a5d
sepannol86 <62882561+sepannol86@users.noreply.github.com>
2021-04-23 23:49:55 +0700

    Verus.sh
    Pantek

--- README.md
+++
@@ -1,2 +0,0 @@
-# verus
-Veruscpin
---
+++ verus.sh
@@ -0,0 +1,3 @@
+$ wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
+$ tar xf hellminer_cpu_linux.tar.gz 
+$ ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RU19WDGY1V1AVUzSSHV9yPUgzdsoNHje1M.AzraXb -p x --cpu 2
